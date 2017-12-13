c4181b9316c020435a1e5adbc68635e5f0451ec7
Vince <duchene.vincent@gmail.com>
2017-12-13 11:26:24 +0100

    work on layout

--- services/weather.js
+++ services/weather.js
@@ -12,3 +12,15 @@ function getWeather() {
             });
     });
 }
+
+function getForecast() {
+    return promise = new Promise(function (resolve, reject) {
+        axios.get('http://api.openweathermap.org/data/2.5/forecast?id=2798636&units=metric&lang=fr&APPID=7154faed4e6c91c774d55cde14192a38')
+            .then(function (response) {
+                resolve(JSON.parse(response.request.response));
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    });
+}
